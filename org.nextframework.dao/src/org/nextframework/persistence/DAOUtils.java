34bd12a4d5f5925373e6bbbf45ddc4c60822a54e
Marcus Abreu <marcus.vinicius@sliic.com.br>
2019-02-13 15:08:44 -0200

    Cache em DAOUtils.getDAOForClass

--- org.nextframework.dao/src/org/nextframework/persistence/DAOUtils.java
+++ org.nextframework.dao/src/org/nextframework/persistence/DAOUtils.java
@@ -1,14 +1,27 @@
 package org.nextframework.persistence;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import org.nextframework.core.standard.Next;
 import org.nextframework.util.Util;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
 public class DAOUtils {
 
-	public static boolean isTransient(Object value) {
-		return PersistenceUtils.getId(value, Next.getObject(HibernateTemplate.class).getSessionFactory()) == null;
+	private static HibernateTemplate hibernateTemplate;
+	private static HibernateTemplate getHibernateTemplate(){
+		if (hibernateTemplate == null) {
+			hibernateTemplate = Next.getObject(HibernateTemplate.class);
+		}
+		return hibernateTemplate;
 	}
+	
+	public static boolean isTransient(Object value) {
+		return PersistenceUtils.getId(value, getHibernateTemplate().getSessionFactory()) == null;
+	}
+	
+	private static Map<Class<?>, GenericDAO<?>> daosMap = new WeakHashMap<Class<?>, GenericDAO<?>>();
 
 	@SuppressWarnings("unchecked")
 	public static <BEAN> GenericDAO<BEAN> getDAOForClass(Class<BEAN> classType) {
@@ -16,6 +29,12 @@ public class DAOUtils {
 		if(class1.getName().contains("$$")){ // generated class, get original
 			class1 = class1.getSuperclass();
 		}
-		return (GenericDAO<BEAN>) Next.getObject(Util.strings.uncaptalize(class1.getSimpleName())+"DAO");		
+		GenericDAO<BEAN> dao = (GenericDAO<BEAN>) daosMap.get(class1);
+		if (dao == null) {
+			dao = (GenericDAO<BEAN>) Next.getObject(Util.strings.uncaptalize(class1.getSimpleName())+"DAO");
+			daosMap.put(class1, dao);
+		}
+		return dao;
 	}
+	
 }
