98ea5a3ec4513a3d9cc29ec8ed72100ba434a027
Marcus Abreu <marcus.vinicius@sliic.com.br>
2019-12-03 15:41:07 -0300

    Minor refactorings

--- org.nextframework.dao/src/org/nextframework/persistence/exception/OracleSQLErrorCodeSQLExceptionTranslator.java
+++ org.nextframework.dao/src/org/nextframework/persistence/exception/OracleSQLErrorCodeSQLExceptionTranslator.java
@@ -44,17 +44,17 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 
 public class OracleSQLErrorCodeSQLExceptionTranslator extends SQLErrorCodeSQLExceptionTranslator {
-	 
+
 	static String errorRegex = "ORA-(?:\\d*?): restrição de integridade \\((.*)?\\) violada - registro filho localizado\n";
 	static String errorRegexIngles = "ORA-(?:\\d*?): integrity constraint \\((.*)?\\) violated - child record found\n";
-//	static String errorRegex = "(.*) statement conflicted with COLUMN REFERENCE constraint '(.*?)'. The conflict occurred in database '(?:.*?)', table '(.*?)', column '(.*?)'.";
+	//static String errorRegex = "(.*) statement conflicted with COLUMN REFERENCE constraint '(.*?)'. The conflict occurred in database '(?:.*?)', table '(.*?)', column '(.*?)'.";
 	static Pattern pattern = Pattern.compile(errorRegex);
 	static Pattern patternIngles = Pattern.compile(errorRegexIngles);
-	
+
 	static final Log log = LogFactory.getLog(OracleSQLErrorCodeSQLExceptionTranslator.class);
-	
+
 	private DataSource dataSource;
-	
+
 	public DataSource getDataSource() {
 		return dataSource;
 	}
@@ -64,121 +64,124 @@ public class OracleSQLErrorCodeSQLExceptionTranslator extends SQLErrorCodeSQLExc
 		super.setDataSource(dataSource);
 		this.dataSource = dataSource;
 	}
-	
+
 	private String getTableName(Class<?> entityClass) {
 		ReflectionCache reflectionCache = ReflectionCacheFactory.getReflectionCache();
 		Table table = reflectionCache.getAnnotation(entityClass, Table.class);
-		if(table != null){
+		if (table != null) {
 			return table.name().toUpperCase();
 		}
 		return entityClass.getSimpleName().toUpperCase();
 	}
-	
+
 	public static class ApplicationDatabaseException extends DataAccessException {
 
-		/**
-		 * 
-		 */
 		private static final long serialVersionUID = 1L;
 
 		public ApplicationDatabaseException(String msg) {
 			super(msg);
 		}
-		
+
 	}
 
 	@Override
 	protected DataAccessException customTranslate(String task, String sql, SQLException sqlEx) {
+
 		//TODO ARRUMAR (FAZER HIGH COHESION.. LOW COUPLING)
-		//System.out.println(task+" - "+sql);
-		if(sqlEx.getNextException() != null){
+		if (sqlEx.getNextException() != null) {
 			sqlEx = sqlEx.getNextException();
 		}
 		String errorMessage = sqlEx.getMessage();
+
 		Matcher matcher = pattern.matcher(errorMessage);
 		Matcher matcherIngles = patternIngles.matcher(errorMessage);
-		//System.out.println(">>> "+errorMessage);
-		if(!matcher.find()){
+		if (!matcher.find()) {
 			matcher = matcherIngles;
 		} else {
 			matcher.reset();
 		}
-		if(matcher.find()){
-			//exceção de FK
+
+		if (matcher.find()) {
+
 			String fk_name = matcher.group(1);
-			if(fk_name.contains(".")){
+			if (fk_name.contains(".")) {
 				fk_name = fk_name.substring(fk_name.indexOf('.') + 1, fk_name.length());
 			}
-			
+
 			String fk_table_name = matcher.group(1).toUpperCase();
 			String pk_table_name = matcher.group(1).toUpperCase();
 			String fkTableDisplayName = null;
 			String pkTableDisplayName = null;
 			Connection connection = null;
+
 			try {
 				connection = dataSource.getConnection();
 				DatabaseMetaData metaData = connection.getMetaData();
-				ResultSet importedKeys = metaData.getImportedKeys(null,null, null);
-				
-				while(importedKeys.next()){
+				ResultSet importedKeys = metaData.getImportedKeys(null, null, null);
+				while (importedKeys.next()) {
 					System.out.println(importedKeys.getString("FK_NAME"));
-					if(importedKeys.getString("FK_NAME").equals(fk_name)){
+					if (importedKeys.getString("FK_NAME").equals(fk_name)) {
 						pk_table_name = importedKeys.getString("PKTABLE_NAME");
-						if(pk_table_name != null){
+						if (pk_table_name != null) {
 							pk_table_name = pk_table_name.toUpperCase();
 						}
-						
+
 						fk_table_name = importedKeys.getString("FKTABLE_NAME");
-						if(fk_table_name != null){
+						if (fk_table_name != null) {
 							fk_table_name = fk_table_name.toUpperCase();
 						}
 					}
 				}
 			} catch (SQLException e) {
-				//se nao conseguir o metadata .. vazar
 				log.warn("Não foi possível conseguir o metadata do banco para ler informacoes de FK.");
 				return null;
 			} finally {
-				if(connection != null){
+				if (connection != null) {
 					try {
 						connection.close();
 					} catch (SQLException e) {
-						
+
 					}
 				}
 			}
-			
+
 			Class<?>[] entities = ClassManagerFactory.getClassManager().getClassesWithAnnotation(Entity.class);
 			pkTableDisplayName = pk_table_name;
 			fkTableDisplayName = fk_table_name;
 			for (Class<?> entityClass : entities) {
 				String tableName = getTableName(entityClass);
-				if(tableName.equals(pk_table_name)){
+				if (tableName.equals(pk_table_name)) {
 					pkTableDisplayName = BeanDescriptorFactory.forClass(entityClass).getDisplayName();
 				}
-				if(tableName.equals(fk_table_name)){
+				if (tableName.equals(fk_table_name)) {
 					fkTableDisplayName = BeanDescriptorFactory.forClass(entityClass).getDisplayName();
 				}
 			}
-			
-			String mensagem = null;
-			if(sql.toLowerCase().trim().startsWith("delete")){
-				mensagem = "Não foi possível remover "+pkTableDisplayName+". Existe(m) registro(s) vinculado(s) em "+fkTableDisplayName+".";
-			} else if(sql.toLowerCase().trim().startsWith("update")){
-				mensagem = "Não foi possível atualizar "+fkTableDisplayName+". A referência para "+pkTableDisplayName+" é inválida.";
-			} else if(sql.toLowerCase().trim().startsWith("insert")){
-				mensagem = "Não foi possível inserir "+fkTableDisplayName+". A referência para "+pkTableDisplayName+" é inválida.";
+
+			String defaultMensagem;
+			if (sql.toLowerCase().trim().startsWith("delete")) {
+				defaultMensagem = "Não foi possível remover " + pkTableDisplayName + ". Existe(m) registro(s) vinculado(s) em " + fkTableDisplayName + ".";
+			} else if (sql.toLowerCase().trim().startsWith("update")) {
+				defaultMensagem = "Não foi possível atualizar " + fkTableDisplayName + ". A referência para " + pkTableDisplayName + " é inválida.";
+			} else if (sql.toLowerCase().trim().startsWith("insert")) {
+				defaultMensagem = "Não foi possível inserir " + fkTableDisplayName + ". A referência para " + pkTableDisplayName + " é inválida.";
+			} else {
+				defaultMensagem = "Não foi possível efetuar operação em " + pkTableDisplayName + ". Existe(m) registro(s) vinculado(s) em " + fkTableDisplayName + ".";
 			}
-			return new ForeignKeyException(mensagem);
+
+			return new ForeignKeyException(defaultMensagem);
+
 		} else {
+
 			int indexOf = errorMessage.indexOf("APP");
-			if(indexOf > 0){
-				errorMessage = errorMessage.substring(indexOf+3);
+			if (indexOf > 0) {
+				errorMessage = errorMessage.substring(indexOf + 3);
 				return new ApplicationDatabaseException(errorMessage);
 			}
+
 		}
+
 		return null;
 	}
-	
 
 }
