f26df8e5f07b9b6e7a12bc9f36794a6a450e7e97
Marcus Abreu <marcus.vinicius@sliic.com.br>
2019-12-03 15:41:07 -0300

    Minor refactorings

--- org.nextframework.dao/src/org/nextframework/persistence/exception/ForeignKeyException.java
+++ org.nextframework.dao/src/org/nextframework/persistence/exception/ForeignKeyException.java
@@ -8,20 +8,13 @@ import org.springframework.dao.DataAccessException;
 public class ForeignKeyException extends DataAccessException {
 
 	private static final long serialVersionUID = 1L;
-	
-	private String msg;
 
-	public ForeignKeyException(String msg) {
-		super(msg);
-		this.msg = msg;
-	}
-	
-	public ForeignKeyException(String msg, Throwable cause) {
-		super(msg, cause);
-		this.msg = msg;
+	public ForeignKeyException(String defaultMensagem) {
+		super(defaultMensagem);
 	}
 
-	public String getOriginalMessage() {
-		return msg;
+	public ForeignKeyException(String defaultMensagem, Throwable cause) {
+		super(defaultMensagem, cause);
 	}
+
 }
