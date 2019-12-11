1a5b17eed43775148cd8a59f5549360ec61e61fc
Marcus Abreu <marcus.vinicius@sliic.com.br>
2019-12-11 17:34:50 -0300

    MessageResolver now contains its Locale.

--- org.nextframework.context/src/org/nextframework/message/MessageResolverFactory.java
+++ org.nextframework.context/src/org/nextframework/message/MessageResolverFactory.java
@@ -36,20 +36,29 @@ public class MessageResolverFactory {
 			this.locale = locale;
 		}
 
+		@Override
+		public Locale getLocale() {
+			return locale;
+		}
+
+		@Override
 		public String message(String code) {
 			return messageSource.getMessage(code, null, locale);
 		}
 
+		@Override
 		public String message(String code, Object[] args) {
 			Object[] args2 = convertArgs(args);
 			return messageSource.getMessage(code, args2, locale);
 		}
 
+		@Override
 		public String message(String code, Object[] args, String defaultValue) {
 			Object[] args2 = convertArgs(args);
 			return messageSource.getMessage(code, args2, defaultValue, locale);
 		}
 
+		@Override
 		public String message(MessageSourceResolvable resolvable) {
 			Object[] stringArgs = convertArgs(resolvable.getArguments());
 			MessageSourceResolvable resolvable2 = Util.objects.newMessage(resolvable.getCodes()[0], stringArgs, resolvable.getDefaultMessage());
