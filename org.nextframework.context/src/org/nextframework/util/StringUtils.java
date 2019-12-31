e8b2c3154197e9027d7d19cd7aaa8c840d826267
Marcus Abreu <marcus.vinicius@sliic.com.br>
2019-12-31 10:45:59 -0300

    Minor refactorings

--- org.nextframework.context/src/org/nextframework/util/StringUtils.java
+++ org.nextframework.context/src/org/nextframework/util/StringUtils.java
@@ -34,7 +34,6 @@ import java.util.Map;
 
 import org.nextframework.bean.BeanDescriptor;
 import org.nextframework.bean.BeanDescriptorFactory;
-import org.nextframework.context.NotInNextContextException;
 
 /**
  * @author rogelgarcia | marcusabreu
@@ -43,18 +42,18 @@ import org.nextframework.context.NotInNextContextException;
  */
 public class StringUtils {
 
-	public String uncaptalize(String str){
+	public String uncaptalize(String str) {
 		return org.springframework.util.StringUtils.uncapitalize(str);
 	}
-	
-	public String captalize(String str){
+
+	public String captalize(String str) {
 		return org.springframework.util.StringUtils.capitalize(str);
 	}
 
 	public boolean isEmpty(String str) {
 		return str == null || str.equals("") || str.trim().equals("");
 	}
-	
+
 	public boolean isNotEmpty(String str) {
 		return !isEmpty(str);
 	}
@@ -66,15 +65,15 @@ public class StringUtils {
 	 */
 	public Map<String, String> parseParameters(String parameters) {
 		HashMap<String, String> hashMap = new HashMap<String, String>();
-		if(parameters == null){
-			
+		if (parameters == null) {
+
 			return hashMap;
 		}
 		String[] split = parameters.split(";");
 		for (String string : split) {
 			String[] split2 = string.split("=");
-			if(split2.length != 2){
-				throw new RuntimeException("Parametros inv·lidos: "+parameters);
+			if (split2.length != 2) {
+				throw new RuntimeException("Parametros inv·lidos: " + parameters);
 			}
 			hashMap.put(split2[0], split2[1]);
 		}
@@ -87,40 +86,39 @@ public class StringUtils {
 	 * @return
 	 */
 	public String separateOnCase(String string) {
-		if(string.length() <= 1){
+		if (string.length() <= 1) {
 			return string;
 		}
 		char[] toCharArray = string.substring(1).toCharArray();
 		StringBuilder builder = new StringBuilder();
 		builder.append(string.charAt(0));
 		for (char c : toCharArray) {
-			if(Character.isUpperCase(c)){
+			if (Character.isUpperCase(c)) {
 				builder.append(" ");
 			}
 			builder.append(c);
 		}
 		return builder.toString();
 	}
-	
-	
+
 	/**
 	 * Cria uma string com o nome da classe e o parametro id
 	 * Ex.: pacote.Classe[campoid=5]
 	 * @param o
 	 * @return
 	 */
-	public String toStringIdStyled(Object o){
+	public String toStringIdStyled(Object o) {
 		return toStringIdStyled(o, false);
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public String toStringIdStyled(Object o, boolean includeDescription){
-		if(o == null){
+	public String toStringIdStyled(Object o, boolean includeDescription) {
+		if (o == null) {
 			return "<null>";
 		}
 		BeanDescriptor beanDescriptor = BeanDescriptorFactory.forBean(o);
 		String idProperty = beanDescriptor.getIdPropertyName();
-		if(idProperty == null){
+		if (idProperty == null) {
 			//CODIGO INSERIDO EM 16 DE NOVEMBRO DE 2006 PARA O AJAX SUPORTAR LISTAS QUE NAO S√O DE BEANS (EX LISTA DE INTEIROS)		
 			if (o.getClass().isEnum()) {
 				Enum e = (Enum) o;
@@ -128,27 +126,27 @@ public class StringUtils {
 			}
 			return o.toString();
 		}
-		
+
 		Object value = beanDescriptor.getId();
-		if(value == null){
+		if (value == null) {
 			value = "<null>";
 		}
-			
+
 		StringBuilder stringBuilder = new StringBuilder();
 		Class clazz = o.getClass();
-		while(clazz.getName().contains("$$")){
+		while (clazz.getName().contains("$$")) {
 			clazz = clazz.getSuperclass();
 		}
 		stringBuilder.append(clazz.getName());
 		stringBuilder.append("[");
-		
+
 		stringBuilder.append(idProperty);
 		stringBuilder.append("=");
 		stringBuilder.append(value);
-		
-		if(includeDescription){
+
+		if (includeDescription) {
 			Object description = beanDescriptor.getDescription();
-			if(description != null){
+			if (description != null) {
 				String descriptionPropertyName = beanDescriptor.getDescriptionPropertyName();
 				stringBuilder.append(";");//TODO refactor. defined in ServletRequestDataBinderNext
 				stringBuilder.append(descriptionPropertyName);
@@ -163,11 +161,15 @@ public class StringUtils {
 	public String toStringDescription(Object value) {
 		return toStringDescription(value, null, null);
 	}
-	
+
 	public String toStringDescription(Object value, String formatDate, String formatNumber) {
 
 		if (value == null) {
 			return "";
+		}
+
+		if (value instanceof String) {
+			return (String) value;
 		}
 
 		formatDate = formatDate == null ? "dd/MM/yyyy" : formatDate;
@@ -177,9 +179,7 @@ public class StringUtils {
 			value = ((Calendar) value).getTime();
 		}
 
-		if (value instanceof String) {
-			return (String) value;
-		} else if (value instanceof Date) { //FIXME
+		if (value instanceof Date) { //FIXME
 			DateFormat dateFormat = new SimpleDateFormat(formatDate);
 			return dateFormat.format(value);
 		} else if (value instanceof Number) {
@@ -214,55 +214,55 @@ public class StringUtils {
 		return description != null ? description.toString() : "";
 	}
 
-    public String removeAccents(String string) {
-    	//copied from apache commons lang
-    	String str = string;
-    	String searchChars = "¡…Õ”⁄¿»Ã“Ÿ¬ Œ‘€ƒÀœ÷‹√’«·ÈÌÛ˙‡ËÏÚ˘‚ÍÓÙ˚‰ÎÔˆ¸„ıÁ"; 
-    	String replaceChars = "AEIOUAEIOUAEIOUAEIOUAOCaeiouaeiouaeiouaeiouaoc";
-    	if (isEmpty(str) || isEmpty(searchChars)) {
-            return str;
-        }
-        boolean modified = false;
-        int replaceCharsLength = replaceChars.length();
-        int strLength = str.length();
-        StringBuilder buf = new StringBuilder(strLength);
-        for (int i = 0; i < strLength; i++) {
-            char ch = str.charAt(i);
-            int index = searchChars.indexOf(ch);
-            if (index >= 0) {
-                modified = true;
-                if (index < replaceCharsLength) {
-                    buf.append(replaceChars.charAt(index));
-                }
-            } else {
-                buf.append(ch);
-            }
-        }
-        if (modified) {
-            return buf.toString();
-        }
-        return str;
+	public String removeAccents(String string) {
+		//copied from apache commons lang
+		String str = string;
+		String searchChars = "¡…Õ”⁄¿»Ã“Ÿ¬ Œ‘€ƒÀœ÷‹√’«·ÈÌÛ˙‡ËÏÚ˘‚ÍÓÙ˚‰ÎÔˆ¸„ıÁ";
+		String replaceChars = "AEIOUAEIOUAEIOUAEIOUAOCaeiouaeiouaeiouaeiouaoc";
+		if (isEmpty(str) || isEmpty(searchChars)) {
+			return str;
+		}
+		boolean modified = false;
+		int replaceCharsLength = replaceChars.length();
+		int strLength = str.length();
+		StringBuilder buf = new StringBuilder(strLength);
+		for (int i = 0; i < strLength; i++) {
+			char ch = str.charAt(i);
+			int index = searchChars.indexOf(ch);
+			if (index >= 0) {
+				modified = true;
+				if (index < replaceCharsLength) {
+					buf.append(replaceChars.charAt(index));
+				}
+			} else {
+				buf.append(ch);
+			}
+		}
+		if (modified) {
+			return buf.toString();
+		}
+		return str;
 //        return org.apache.commons.lang.StringUtils.replaceChars(string, "¡…Õ”⁄¿»Ã“Ÿ¬ Œ‘€ƒÀœ÷‹√’«·ÈÌÛ˙‡ËÏÚ˘‚ÍÓÙ˚‰ÎÔˆ¸„ıÁ", "AEIOUAEIOUAEIOUAEIOUAOCaeiouaeiouaeiouaeiouaoc");
-    }
+	}
 
 	public String toString(Object o) {
-		if(o == null){
+		if (o == null) {
 			return null;
 		} else {
-			if(o instanceof String){
+			if (o instanceof String) {
 				return (String) o;
-			} else if(o instanceof Date){
+			} else if (o instanceof Date) {
 				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 				return dateFormat.format(o);
-			} else if(o instanceof Double){
-				return new DecimalFormat("#,##0.00").format((Double)o);
+			} else if (o instanceof Double) {
+				return new DecimalFormat("#,##0.00").format((Double) o);
 			}
 			return o.toString();
 		}
 	}
 
 	public String toString(Object o, String def) {
-		if(o == null){
+		if (o == null) {
 			return def;
 		} else {
 			return o.toString();
@@ -275,23 +275,24 @@ public class StringUtils {
 	 * @return
 	 */
 	public String escape(String string) {
-		if(string == null){
+		if (string == null) {
 			return "";
 		}
 		return string.replace("\\", "\\\\").replace("'", "\\'").replace("\n", "\\n");
 	}
-	
+
 	/**
 	 * Faz o escape de aspas duplas
 	 * @param string
 	 * @return
 	 */
 	public String escapeQuotes(String string) {
-		if(string == null){
+		if (string == null) {
 			return "";
 		}
 		return string.replace("\\", "\\\\").replace("\"", "\\\"").replace("\r\n", "\\n").replace("\n", "\\n");
 	}
+
 	/**
 	 * Faz escape de aspas duplas e remove new lines
 	 * @param string
@@ -300,40 +301,40 @@ public class StringUtils {
 	public String escapeText(String string) {
 		return string.replace("\\", "\\\\").replace("\"", "\\\"").replace('\r', ' ').replace('\n', ' ');
 	}
-	
+
 	public static String onlyNumbers(String s) {
 		int i;
-		String numeros  = "0123456789";
-		int tamanho     = s.length();			
+		String numeros = "0123456789";
+		int tamanho = s.length();
 		StringBuilder sb = new StringBuilder();
-		String caractere= "";		
+		String caractere = "";
 		for (i = 0; i < tamanho; i++) {
-			caractere = s.substring(i,i + 1);
+			caractere = s.substring(i, i + 1);
 			if (numeros.indexOf(caractere) != -1)
 				sb.append(caractere);
-		} 
+		}
 		return sb.toString();
 	} //SoNumero
-	
+
 	public String stringCheia(String stringOriginal, String caractereCompleta, int tamanho, boolean direita) {
-		if (stringOriginal == null){
+		if (stringOriginal == null) {
 			stringOriginal = "";
 		}
 		String result = stringOriginal;
-		if (caractereCompleta == null){
+		if (caractereCompleta == null) {
 			caractereCompleta = "";
 		}
-		if (caractereCompleta.equals("")) 
-			caractereCompleta += "0";		
+		if (caractereCompleta.equals(""))
+			caractereCompleta += "0";
 		while (result.length() < tamanho) {
-			if (direita) 
+			if (direita)
 				result += caractereCompleta;
 			else
-				result  = caractereCompleta + result;
+				result = caractereCompleta + result;
 		}
 		if (result.length() > tamanho) {
-			if (direita) 
-				result = result.substring(0,tamanho);
+			if (direita)
+				result = result.substring(0, tamanho);
 			else
 				result = result.substring(result.length() - tamanho, result.length());
 		}
@@ -341,7 +342,7 @@ public class StringUtils {
 	} //StringCheia
 
 	public String emptyIfNull(String propertyPrefix) {
-		return propertyPrefix != null? propertyPrefix : "";
+		return propertyPrefix != null ? propertyPrefix : "";
 	}
 
 }
