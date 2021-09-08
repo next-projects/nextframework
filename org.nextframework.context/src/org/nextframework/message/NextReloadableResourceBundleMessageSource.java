90fba1ab80d7a1e43fe27140e9de1536388959ed
Marcus Abreu <marcus.vinicius@sliic.com.br>
2021-09-08 16:57:31 -0300

    NextReloadableResourceBundleMessageSource can now read wildcards

--- org.nextframework.context/src/org/nextframework/message/NextReloadableResourceBundleMessageSource.java
+++ org.nextframework.context/src/org/nextframework/message/NextReloadableResourceBundleMessageSource.java
@@ -3,13 +3,71 @@ package org.nextframework.message;
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-public class NextReloadableResourceBundleMessageSource extends ReloadableResourceBundleMessageSource {
+public class NextReloadableResourceBundleMessageSource extends ReloadableResourceBundleMessageSource implements ResourceLoaderAware {
+
+	private long cacheMillis2 = -1;
+
+	//https://www.tabnine.com/web/assistant/code/rs/5c66e35d1095a50001e73891#L21
+	private static final String PROPERTIES_SUFFIX = ".properties";
+	private final ConcurrentMap<String, PropertiesHolder> cachedClasspathProperties = new ConcurrentHashMap<String, PropertiesHolder>();
+	private PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
 	private String[] optionalPrefixes = new String[0];
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		if (resourceLoader != null) {
+			resolver = new PathMatchingResourcePatternResolver(resourceLoader);
+		}
+	}
+
+	@Override
+	protected PropertiesHolder refreshProperties(String filename, PropertiesHolder propHolder) {
+		if (isPattern(filename)) {
+			PropertiesHolder existingHolder = this.cachedClasspathProperties.get(filename);
+			if (existingHolder != null && existingHolder.getRefreshTimestamp() > (System.currentTimeMillis() - cacheMillis2)) {
+				return existingHolder;
+			}
+			return refreshClassPathProperties(filename, propHolder);
+		} else {
+			return super.refreshProperties(filename, propHolder);
+		}
+	}
+
+	private boolean isPattern(String filename) {
+		return filename.startsWith(PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX) ||
+				resolver.getPathMatcher().isPattern(filename);
+	}
+
+	private PropertiesHolder refreshClassPathProperties(String filename, PropertiesHolder propHolder) {
+		Properties properties = new Properties();
+		long lastModified = -1;
+		try {
+			Resource[] resources = resolver.getResources(filename + PROPERTIES_SUFFIX);
+			for (Resource resource : resources) {
+				String sourcePath = resource.getURI().toString().replace(PROPERTIES_SUFFIX, "");
+				PropertiesHolder holder = super.refreshProperties(sourcePath, propHolder);
+				properties.putAll(holder.getProperties());
+				if (lastModified < resource.lastModified()) {
+					lastModified = resource.lastModified();
+				}
+			}
+		} catch (IOException ignored) {
+		}
+		PropertiesHolder holder = new PropertiesHolder(properties, lastModified);
+		holder.setRefreshTimestamp(cacheMillis2 < 0 ? -1 : System.currentTimeMillis());
+		cachedClasspathProperties.put(filename, holder);
+		return holder;
+	}
 
 	@Override
 	protected Properties loadProperties(Resource resource, String filename) throws IOException {
@@ -48,12 +106,23 @@ public class NextReloadableResourceBundleMessageSource extends ReloadableResourc
 		return null;
 	}
 
+	@Override
+	public void clearCache() {
+		super.clearCache();
+		this.cachedClasspathProperties.clear();
+	}
+
 	public String[] getOptionalPrefixes() {
 		return optionalPrefixes;
 	}
 
 	public void setOptionalPrefixes(String[] optionalPrefixes) {
 		this.optionalPrefixes = optionalPrefixes;
+	}
+
+	public void setCacheSeconds(int cacheSeconds) {
+		super.setCacheSeconds(cacheSeconds);
+		this.cacheMillis2 = (cacheSeconds * 1000);
 	}
 
 }
