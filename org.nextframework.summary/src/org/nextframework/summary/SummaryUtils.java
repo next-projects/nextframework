35c1f4a126d9df918272f1cf18c00ed80a29af0b
Marcus Abreu <marcus.vinicius@sliic.com.br>
2024-03-21 17:09:14 -0300

    ReportBuilderSourceGenerator fix

--- org.nextframework.summary/src/org/nextframework/summary/SummaryUtils.java
+++ org.nextframework.summary/src/org/nextframework/summary/SummaryUtils.java
@@ -4,32 +4,32 @@ import org.springframework.util.StringUtils;
 
 public class SummaryUtils {
 
-	public static String convertCompositeGroupToMethodFormat(String scopeGroup){
+	public static String convertCompositeGroupToMethodFormat(String scopeGroup) {
 		return convertCompositeGroupToMethodFormat(scopeGroup, false);
 	}
-	
-	public static String convertCompositeGroupToMethodFormat(String scopeGroup, boolean ignoreSummaryName){
+
+	public static String convertCompositeGroupToMethodFormat(String scopeGroup, boolean ignoreSummaryName) {
 		// the group can be configured in the format foo.bar
 		// however composite groups are compared with foo_Bar in the inner workings
-		if(scopeGroup.indexOf('.') > 0){
+		if (scopeGroup.indexOf('.') > 0) {
 			String[] parts = scopeGroup.split("\\.");
-
 			StringBuilder builder = new StringBuilder();
 			builder.append(parts[0]);
 			int beginFrom = 1; /*1!*/
-			if(parts[0].equals("summary") && ignoreSummaryName){
+			if (parts[0].equals("summary") && ignoreSummaryName) {
 				builder.append(".").append(parts[1]);
 				beginFrom = 2;
 			}
 			for (int i = beginFrom; i < parts.length; i++) {
 				String part = parts[i];
-				
+
 				builder.append("_");
 				builder.append(StringUtils.capitalize(part));
 			}
 			return builder.toString();
 		}
 		return scopeGroup;
-		
+
 	}
+
 }
--- org.nextframework.summary/src/org/nextframework/summary/dynamic/DynamicSummaryImpl.java
+++ org.nextframework.summary/src/org/nextframework/summary/dynamic/DynamicSummaryImpl.java
@@ -18,20 +18,19 @@ import org.nextframework.compilation.JavaSourceCompiler;
 import org.nextframework.compilation.SourceCodeBuilder;
 import org.nextframework.exception.NextException;
 import org.nextframework.summary.Summary;
-import org.nextframework.summary.annotations.CalculationType;
 import org.nextframework.summary.annotations.Group;
 import org.springframework.util.StringUtils;
 
 class DynamicSummaryImpl<E> extends DynamicSummary<E> {
-	
-	static Map<DynamicSummary<?>, WeakReference<Class<Summary<?>>>> cache = Collections.synchronizedMap(new HashMap<DynamicSummary<?>, WeakReference<Class<Summary<?>>>>()); 
-	
-	@SuppressWarnings("unchecked")
+
+	static Map<DynamicSummary<?>, WeakReference<Class<Summary<?>>>> cache = Collections.synchronizedMap(new HashMap<DynamicSummary<?>, WeakReference<Class<Summary<?>>>>());
+
 	@Override
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	protected Class<Summary<E>> getClassFor(DynamicSummary<E> dynamicSummary) {
 		WeakReference<Class<Summary<?>>> ref = cache.get(dynamicSummary);
 		Class class1;
-		if(ref == null || ((class1 = ref.get()) == null)){
+		if (ref == null || ((class1 = ref.get()) == null)) {
 			class1 = createSummaryClass();
 			cache.put(dynamicSummary, new WeakReference<Class<Summary<?>>>(class1));
 		}
@@ -42,15 +41,15 @@ class DynamicSummaryImpl<E> extends DynamicSummary<E> {
 		return createSummaryClass(new URLClassLoader(new URL[0], DynamicSummary.class.getClassLoader()));
 		//return createSummaryClass(DynamicSummary.class.getClassLoader());
 	}
-	
-	public String getSourceCode(){
+
+	public String getSourceCode() {
 		CodeGenerator codeGenerator = new CodeGenerator(this);
 		return codeGenerator.getCode();
 	}
-	
-	public String getSourceCode(String summaryClassName){
+
+	public String getSourceCode(String summaryClassName) {
 		String pack = summaryClassName.substring(0, summaryClassName.lastIndexOf('.'));
-		String className = summaryClassName.substring(summaryClassName.lastIndexOf('.')+1);
+		String className = summaryClassName.substring(summaryClassName.lastIndexOf('.') + 1);
 		CodeGenerator codeGenerator = new CodeGenerator(this, className, pack);
 		return codeGenerator.getCode();
 	}
@@ -68,54 +67,54 @@ class DynamicSummaryImpl<E> extends DynamicSummary<E> {
 		}
 	}
 
-	
 	protected DynamicSummaryImpl(int serialId) {
 		super(serialId);
 	}
-	
-	@SuppressWarnings("unchecked")
-	protected Class getDataClass(){
-		if(Summary.class.isAssignableFrom(getReferenceClass())){
+
+	@SuppressWarnings("rawtypes")
+	protected Class getDataClass() {
+		if (Summary.class.isAssignableFrom(getReferenceClass())) {
 			throw new RuntimeException("not implemented");
 		} else {
 			return getReferenceClass();
 		}
 	}
-	
-	protected Type getSuperClass(){
+
+	@SuppressWarnings("rawtypes")
+	protected Type getSuperClass() {
 		return Summary.class.isAssignableFrom(getReferenceClass()) ? getReferenceClass() : new ParameterizedType() {
-			
+
 			@Override
 			public Type getRawType() {
 				return Summary.class;
 			}
-			
+
 			@Override
 			public Type getOwnerType() {
 				return null;
 			}
-			
+
 			@Override
 			public Type[] getActualTypeArguments() {
-				return new Type[]{getReferenceClass()};
+				return new Type[] { getReferenceClass() };
 			}
-			
-			@SuppressWarnings("unchecked")
+
 			@Override
 			public String toString() {
-				return ((Class)getRawType()).getName()+"<"+((Class)getActualTypeArguments()[0]).getName()+">";
+				return ((Class) getRawType()).getName() + "<" + ((Class) getActualTypeArguments()[0]).getName() + ">";
 			}
-		}; 
+
+		};
 	}
-	
-	@SuppressWarnings("unchecked")
-	protected String getSuperclassString(){
+
+	@SuppressWarnings("rawtypes")
+	protected String getSuperclassString() {
 		Type superClass = getSuperClass();
-		if(superClass instanceof Class){
+		if (superClass instanceof Class) {
 			return ((Class) superClass).getName();
-		} else if(superClass instanceof ParameterizedType) {
-			if(((ParameterizedType) superClass).getRawType().equals(Summary.class)){
-				return "Summary<"+((Class)((ParameterizedType) superClass).getActualTypeArguments()[0]).getName()+">";
+		} else if (superClass instanceof ParameterizedType) {
+			if (((ParameterizedType) superClass).getRawType().equals(Summary.class)) {
+				return "Summary<" + ((Class) ((ParameterizedType) superClass).getActualTypeArguments()[0]).getName() + ">";
 			}
 			return superClass.toString();
 		} else {
@@ -123,21 +122,6 @@ class DynamicSummaryImpl<E> extends DynamicSummary<E> {
 		}
 	}
 
-//	@SuppressWarnings("unchecked")
-//	public static void main(String[] args) {
-//		Class<? extends Summary> summaryClass = DynamicSummary.getInstance(DynamicGroup.class)
-//			.addGroup("name")
-//			.getSummaryClass();
-//		ArrayList rows = new ArrayList();
-//		rows.add(new DynamicGroup("A"));
-//		rows.add(new DynamicGroup("A"));
-//		rows.add(new DynamicGroup("B"));
-//		rows.add(new DynamicGroup("B"));
-//		SummaryResult createFrom = SummaryResult.createFrom(rows, summaryClass);
-//		
-//		TextReportPrinter.print(createFrom.getItems());
-//		//System.out.println(((ParameterizedType)X.class.getGenericSuperclass()).getActualTypeArguments()[0]);
-//	}
 }
 
 class CodeGenerator {
@@ -149,11 +133,11 @@ class CodeGenerator {
 	private BeanDescriptor beanDescriptor;
 
 	public CodeGenerator(DynamicSummaryImpl<?> dynamicSummary) {
-		this(dynamicSummary, "DynamicSummary_"+dynamicSummary.getSerialId());
+		this(dynamicSummary, "DynamicSummary_" + dynamicSummary.getSerialId());
 	}
 
 	public CodeGenerator(DynamicSummaryImpl<?> dynamicSummary, String className) {
-		this(dynamicSummary, className, getPackage(dynamicSummary.getReferenceClass().getName())+".summary");
+		this(dynamicSummary, className, getPackage(dynamicSummary.getReferenceClass().getName()) + ".summary");
 	}
 
 	public CodeGenerator(DynamicSummaryImpl<?> dynamicSummary, String className, String packageName) {
@@ -162,9 +146,9 @@ class CodeGenerator {
 		generatedClassPackage = packageName;
 		generatedClassName = generatedClassPackage + "." + generatedClassSimpleName;
 	}
-	
+
 	private static String getPackage(String name) {
-		if(name.lastIndexOf('.') > 0){
+		if (name.lastIndexOf('.') > 0) {
 			return name.substring(0, name.lastIndexOf('.'));
 		}
 		return name;
@@ -174,17 +158,17 @@ class CodeGenerator {
 		return generatedClassName;
 	}
 
-	public String getCode(){
+	public String getCode() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("package "+generatedClassPackage+";\n\n");
-		builder.append("import "+DisplayName.class.getName()+";\n");
-		builder.append("import "+getPackageName(Summary.class)+".*;\n");
-		builder.append("import "+getPackageName(Group.class)+".*;\n");
-		builder.append("import "+getPackageName(SourceCodeBuilder.class)+".*;\n");
+		builder.append("package " + generatedClassPackage + ";\n\n");
+		builder.append("import " + DisplayName.class.getName() + ";\n");
+		builder.append("import " + getPackageName(Summary.class) + ".*;\n");
+		builder.append("import " + getPackageName(Group.class) + ".*;\n");
+		builder.append("import " + getPackageName(SourceCodeBuilder.class) + ".*;\n");
 		builder.append("import java.text.*;\n");
 		builder.append("import java.util.*;\n");
 		builder.append("\n");
-		builder.append("public class "+generatedClassSimpleName+" extends "+dynamicSummary.getSuperclassString()+" implements AutoGenerated {\n\n");
+		builder.append("public class " + generatedClassSimpleName + " extends " + dynamicSummary.getSuperclassString() + " implements AutoGenerated {\n\n");
 		createGroups(builder);
 		createVariables(builder);
 		createFormatFunction(builder);
@@ -207,82 +191,85 @@ class CodeGenerator {
 
 	private void createVariable(StringBuilder builder, DynamicVariable variable, int i) {
 		BeanDescriptor beanDescriptor = getBeanDescriptor();
-		
+
 		String variableName = variable.getName();
 		builder.append("	/*************************************************************************\n");
-		builder.append("	  variable "+variableName+"\n");
+		builder.append("	  variable " + variableName + "\n");
 		builder.append("	 ************************************************************************/\n\n");
+
 		PropertyDescriptor propertyDescriptor;
 		String type;
-		if(variable.getJavaExpression() == null){
+		if (variable.getJavaExpression() == null) {
 			propertyDescriptor = beanDescriptor.getPropertyDescriptor(variableName);
 			type = getTypeAsString(propertyDescriptor.getType());
 		} else {
 			type = getTypeAsString(variable.getReturnType());
 		}
-		if(!(variable instanceof DynamicVariableDecorator)){
+
+		if (!(variable instanceof DynamicVariableDecorator)) {
 			String[] getters = getGetters(variableName);
-			if(variable.getDisplayName() != null){
-				builder.append("	@DisplayName(\""+variable.getDisplayName()+"\")\n");
+			if (variable.getDisplayName() != null) {
+				builder.append("	@DisplayName(\"" + variable.getDisplayName() + "\")\n");
 			}
-			builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + "(){\n");
-			if(variable.getJavaExpression() != null){
+			builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + "(){\n");
+			if (variable.getJavaExpression() != null) {
 				builder.append("		try {\n");
-				builder.append("			return "+variable.getJavaExpression());
+				builder.append("			return " + variable.getJavaExpression());
 				builder.append(";\n");
 				builder.append("		} catch(RuntimeException e){return null;}\n");
 			} else {
 				builder.append("		try {\n");
 				builder.append("		    return getCurrent()");
 				for (int j = 0; j < getters.length; j++) {
-					builder.append("."+getters[j]+"()");
+					builder.append("." + getters[j] + "()");
 				}
 				builder.append(";\n");
 				builder.append("		} catch(NullPointerException e){return null;}\n");
 			}
 			builder.append("	}\n");
 		} else {
-			if(variable.getDisplayName() != null){
-				builder.append("	@DisplayName(\""+variable.getDisplayName()+"\")\n");
+			if (variable.getDisplayName() != null) {
+				builder.append("	@DisplayName(\"" + variable.getDisplayName() + "\")\n");
 			}
 			DynamicVariableDecorator decorator = (DynamicVariableDecorator) variable;
-			builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + "(){\n");
-			builder.append("		return "+decorator.getJavaExpression()+"("+getCompositeGetterMethodNameFromGroup(decorator.getForVariable())+"());\n");
+			builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + "(){\n");
+			builder.append("		return " + decorator.getJavaExpression() + "(" + getCompositeGetterMethodNameFromGroup(decorator.getForVariable()) + "());\n");
 			builder.append("	}\n");
 		}
-		
-		if(variable.getDisplayName() != null){
-			builder.append("	@DisplayName(\""+variable.getDisplayName()+"\")\n");
+
+		if (variable.getDisplayName() != null) {
+			builder.append("	@DisplayName(\"" + variable.getDisplayName() + "\")\n");
 		}
-		if(variable instanceof DynamicVariableDecorator){
+		if (variable instanceof DynamicVariableDecorator) {
 			DynamicVariableDecorator decorator = (DynamicVariableDecorator) variable;
-			builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + "Report(){\n");
-			builder.append("		return "+decorator.getJavaExpression()+"("+getCompositeGetterMethodNameFromGroup(decorator.getForVariable())+"Report());\n");
+			builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + "Report(){\n");
+			builder.append("		return " + decorator.getJavaExpression() + "(" + getCompositeGetterMethodNameFromGroup(decorator.getForVariable()) + "Report());\n");
 			builder.append("	}\n");
 		} else {
-			builder.append("	@Variable(calculation=CalculationType."+variable.getCalculationType().toString()+", scopeGroup=\"report\")\n");
-			builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + "Report(){\n");
-			builder.append("		return "+getCompositeGetterMethodNameFromGroup(variableName)+"();\n");
+			builder.append("	@Variable(calculation=CalculationType." + variable.getCalculationType().toString() + ", scopeGroup=\"report\")\n");
+			builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + "Report(){\n");
+			builder.append("		return " + getCompositeGetterMethodNameFromGroup(variableName) + "();\n");
 			builder.append("	}\n");
 		}
-		
+
 		DynamicGroup[] groups = dynamicSummary.getGroups();
 		for (DynamicGroup dynamicGroup : groups) {
-			if(variable.getDisplayName() != null){
-				builder.append("	@DisplayName(\""+variable.getDisplayName()+"\")\n");
+			if (variable.getDisplayName() != null) {
+				builder.append("	@DisplayName(\"" + variable.getDisplayName() + "\")\n");
 			}
-			if(variable instanceof DynamicVariableDecorator) {
+			if (variable instanceof DynamicVariableDecorator) {
 				DynamicVariableDecorator decorator = (DynamicVariableDecorator) variable;
-				builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + getCompositeGroupName(dynamicGroup.getName()) + "(){\n");
-				builder.append("		return "+decorator.getJavaExpression()+"("+getCompositeGetterMethodNameFromGroup(decorator.getForVariable()) + getCompositeGroupName(dynamicGroup.getName())+"());\n");
+				builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + getCompositeGroupName(dynamicGroup.getName()) + "(){\n");
+				builder.append("		return " + decorator.getJavaExpression() + "(" + getCompositeGetterMethodNameFromGroup(decorator.getForVariable()) + getCompositeGroupName(dynamicGroup.getName()) + "());\n");
 				builder.append("	}\n");
 			} else {
-				builder.append("	@Variable(calculation=CalculationType."+variable.getCalculationType().toString()+", scopeGroup=\""+dynamicGroup.getName()+"\")\n");
-				builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(variableName) + getCompositeGroupName(dynamicGroup.getName())+"(){\n");
-				builder.append("		return "+getCompositeGetterMethodNameFromGroup(variableName)+"();\n");
+				builder.append("	@Variable(calculation=CalculationType." + variable.getCalculationType().toString() + ", scopeGroup=\"" + dynamicGroup.getName() + "\")\n");
+				builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(variableName) + getCompositeGroupName(dynamicGroup.getName()) + "(){\n");
+				builder.append("		return " + getCompositeGetterMethodNameFromGroup(variableName) + "();\n");
 				builder.append("	}\n");
 			}
 		}
+
 	}
 
 	private void createGroups(StringBuilder builder) {
@@ -296,60 +283,55 @@ class CodeGenerator {
 
 	private void createGroup(StringBuilder builder, DynamicGroup dynamicGroup, int i) {
 		BeanDescriptor beanDescriptor = getBeanDescriptor();
-		
-		
 		String name = dynamicGroup.getName();
-		builder.append("	//group "+name+"\n");
+		builder.append("	//group " + name + "\n");
 		PropertyDescriptor propertyDescriptor = beanDescriptor.getPropertyDescriptor(name);
 		String type = getTypeAsString(propertyDescriptor.getType());
-		if(dynamicGroup.pattern != null){
+		if (dynamicGroup.pattern != null) {
 			type = "String";
 		}
 		String[] getters = getGetters(name);
-		builder.append("	@Group(value="+i+", name=\""+dynamicGroup.getName()+"\")\n");
-		builder.append("	public " + type + " "+ getCompositeGetterMethodNameFromGroup(name) + "(){\n");
+		builder.append("	@Group(value=" + i + ", name=\"" + dynamicGroup.getName() + "\")\n");
+		builder.append("	public " + type + " " + getCompositeGetterMethodNameFromGroup(name) + "(){\n");
 		builder.append("		try {\n");
-		if(dynamicGroup.pattern != null){
-			builder.append("		    return format(\""+escape(dynamicGroup.pattern)+"\", getCurrent()");
+		if (dynamicGroup.pattern != null) {
+			builder.append("		    return format(\"" + escape(dynamicGroup.pattern) + "\", getCurrent()");
 			for (int j = 0; j < getters.length; j++) {
-				builder.append("."+getters[j]+"()");
+				builder.append("." + getters[j] + "()");
 			}
 			builder.append(")");
 		} else {
 			builder.append("		    return getCurrent()");
 			for (int j = 0; j < getters.length; j++) {
-				builder.append("."+getters[j]+"()");
+				builder.append("." + getters[j] + "()");
 			}
 		}
 		builder.append(";\n");
 		builder.append("		} catch(NullPointerException e){return null;}\n");
-		
 		builder.append("	}\n");
 	}
-	
+
 	private String getCompositeGetterMethodNameFromGroup(String name) {
 		StringBuilder builder = getCompositeGroupName(name);
-		return "get"+builder.toString();
+		return "get" + builder.toString();
 	}
 
 	public StringBuilder getCompositeGroupName(String name) {
 		String[] names = getNames(name);
 		StringBuilder builder = new StringBuilder();
-		boolean includeUnderscore = false;
 		for (String var : names) {
-			if(includeUnderscore){
+			if (builder.length() > 0) {
 				builder.append("_");
 			}
 			builder.append(StringUtils.capitalize(var));
-			includeUnderscore = true;
 		}
 		return builder;
 	}
 
 	private void createFormatFunction(StringBuilder builder) {
 		builder.append("	public String format(String format, Object value){\n");
-		builder.append("	    if(value instanceof Calendar)\n");
-		builder.append("	    	value = ((Calendar)value).getTime();\n");
+		builder.append("		if(value instanceof Calendar)\n");
+		builder.append("			value = ((Calendar)value).getTime();\n");
 		builder.append("		if(value instanceof Date)\n"); //FIXME
 		builder.append("			return new SimpleDateFormat(format).format(value);\n");
 		builder.append("		if(value instanceof Number)\n");
@@ -365,7 +347,7 @@ class CodeGenerator {
 	}
 
 	private BeanDescriptor getBeanDescriptor() {
-		if(beanDescriptor == null){
+		if (beanDescriptor == null) {
 			beanDescriptor = BeanDescriptorFactory.forClass(dynamicSummary.getDataClass());
 		}
 		return beanDescriptor;
@@ -388,14 +370,14 @@ class CodeGenerator {
 		return BeanDescriptorUtils.getGetterFromProperty(name);
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings("rawtypes")
 	private String getTypeAsString(Type type) {
-		if(type instanceof Class){
-			Class clazz = (Class)type;
-			if(clazz.isArray()){
+		if (type instanceof Class) {
+			Class clazz = (Class) type;
+			if (clazz.isArray()) {
 				return getTypeAsString(clazz.getComponentType()) + "[]";
 			}
-			if(clazz.getName().startsWith("java.lang")){
+			if (clazz.getName().startsWith("java.lang")) {
 				return clazz.getSimpleName();
 			} else {
 				return clazz.getName();
@@ -404,5 +386,5 @@ class CodeGenerator {
 			return type.toString();
 		}
 	}
-	
+
 }
