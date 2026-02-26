34900e2f3464a216e4a69c4a459427d078385db5
Marcus Abreu <marcus.vinicius@sliic.com.br>
2026-02-26 10:07:42 -0300

    Recopying old Hibernate classes. Something went wrong earlier...

--- org.nextframework.dao/src/org/springframework/orm/hibernate4/SessionFactoryUtils.java
+++ org.nextframework.dao/src/org/springframework/orm/hibernate4/SessionFactoryUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.orm.hibernate4;
 
 import java.lang.reflect.Method;
@@ -85,7 +101,9 @@ public abstract class SessionFactoryUtils {
 	public static DataSource getDataSource(SessionFactory sessionFactory) {
 		if (getConnectionProviderMethod != null && sessionFactory instanceof SessionFactoryImplementor) {
 			Wrapped cp = (Wrapped) ReflectionUtils.invokeMethod(getConnectionProviderMethod, sessionFactory);
-			return cp.unwrap(DataSource.class);
+			if (cp != null) {
+				return cp.unwrap(DataSource.class);
+			}
 		}
 		return null;
 	}
